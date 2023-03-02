8e44ecd55413b950f06fa52072e03f8b4421fc82
GreyGoo <21207197+IGreyGooI@users.noreply.github.com>
2023-03-03 04:29:11 +0800

    reflect for extent3i
    Co-authored-by: Theia Macphail <classic-sasquatch-behavior@users.noreply.github.com>

--- crates/building_blocks_core/src/reflect.rs
+++ crates/building_blocks_core/src/reflect.rs
@@ -1,6 +1,12 @@
-use bevy_reflect::{impl_from_reflect_value, impl_reflect_value, ReflectSerialize, ReflectDeserialize, std_traits::ReflectDefault};
+use bevy_reflect::{
+    impl_from_reflect_value, impl_reflect_value, std_traits::ReflectDefault, ReflectDeserialize,
+    ReflectSerialize,
+};
 
-use crate::Point3i;
+use crate::{Extent3i, Point3i};
 
 impl_reflect_value!(Point3i(Debug, PartialEq, Serialize, Deserialize, Default));
 impl_from_reflect_value!(Point3i);
+
+impl_reflect_value!(Extent3i(Debug, PartialEq, Serialize, Deserialize));
+impl_from_reflect_value!(Extent3i);
